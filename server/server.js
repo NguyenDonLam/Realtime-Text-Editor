441b05696416370cd11128780225eb437ccf5f1e
NguyenDonLam <donlamn@student.unimelb.edu.au>
2024-11-09 19:32:21 +1100

    feat: Add persistency between clients

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 const io = require("socket.io")(3001 , {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "http://localhost:5173",
         methods: ["GET", "POST"]
     }
 })
 
 io.on("connection", socket => {
-    
+    socket.on("send-changes", delta => {
+        socket.broadcast.emit("recieve-changes", delta)
+    })
 })
