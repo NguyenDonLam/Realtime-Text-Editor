9aa8032e3a13827dad2e52c668c4a8409c4a12b7
NguyenDonLam <donlamn@student.unimelb.edu.au>
2024-11-09 20:28:38 +1100

    fix: Now Buildable

--- client/src/component/TextEditor.tsx
+++ client/src/component/TextEditor.tsx
@@ -1,83 +1,85 @@
 import Quill from "quill";
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import "quill/dist/quill.snow.css";
-import "../styles/TextEditorStyles.css"
+import "../styles/TextEditorStyles.css";
 import { io, Socket } from "socket.io-client";
 import { useParams } from "react-router";
-
+import { Delta } from "quill/core";
 
 const TOOLBAR_OPTIONS = [
-    [{header: [1, 2, 3, 4, 5, 6, false]}],
-    [{font: []}],
-    [{list: "ordered"}, {list: "bullet"}],
-    ["bold", "italic", "underline"],
-    [{color: []}, {background: []}],
-    [{script: "sub"}, {script:"super"}],
-    [{align: []}],
-    ["image", "blockquote", "code-block"],
-    ["clean"],
-]
+  [{ header: [1, 2, 3, 4, 5, 6, false] }],
+  [{ font: [] }],
+  [{ list: "ordered" }, { list: "bullet" }],
+  ["bold", "italic", "underline"],
+  [{ color: [] }, { background: [] }],
+  [{ script: "sub" }, { script: "super" }],
+  [{ align: [] }],
+  ["image", "blockquote", "code-block"],
+  ["clean"],
+];
 export default function TextEditor() {
-    const {id: documentId} = useParams();
-    const [socket, setSocket] = useState<Socket>();
-    const [quill, setQuill] = useState<Quill>();
-    useEffect(()=> {
-        const s = io("http://localhost:3001");
-        setSocket(s)
-        return () => {
-            s.disconnect()
-        }
-    }, [])
+  const { id: documentId } = useParams();
+  const [socket, setSocket] = useState<Socket>();
+  const [quill, setQuill] = useState<Quill>();
+  useEffect(() => {
+    const s = io("http://localhost:3001");
+    setSocket(s);
+    return () => {
+      s.disconnect();
+    };
+  }, []);
 
-    useEffect(() => {
-        if (socket == null || quill == null) return
+  useEffect(() => {
+    if (socket == null || quill == null) return;
 
-        socket.once("load-document", document => {
-            quill.setContents(document);
-            quill.enable();
-        })
+    socket.once("load-document", (document) => {
+      quill.setContents(document);
+      quill.enable();
+    });
 
-        socket.emit("get-document", documentId)
-    }, [socket, quill, documentId])
+    socket.emit("get-document", documentId);
+  }, [socket, quill, documentId]);
 
-    // Upon recieving the events
-    useEffect(() => {
-          if (socket == null || quill == null) return;
+  // Upon recieving the events
+  useEffect(() => {
+    if (socket == null || quill == null) return;
 
-          const handler = (delta) => {
-            quill.updateContents(delta);
-          };
-          socket.on("recieve-changes", handler);
+    const handler = (delta: Delta) => {
+      quill.updateContents(delta);
+    };
+    socket.on("recieve-changes", handler);
 
-          return () => {
-            socket.off("recieve-changes", handler);
-          };
-    }, [socket, quill]);
+    return () => {
+      socket.off("recieve-changes", handler);
+    };
+  }, [socket, quill]);
 
-    // Upon changing doc
-    useEffect(() => {
-        if (socket == null || quill == null) return;
-        const handler = (delta, oldDelta, source) => {
-          if (source != "user") return;
-          socket.emit("send-changes", delta);
-        };
-        quill.on("text-change", handler)
+  // Upon changing doc
+  useEffect(() => {
+    if (socket == null || quill == null) return;
+    const handler = (delta: Delta, source: string) => {
+      if (source != "user") return;
+      socket.emit("send-changes", delta);
+    };
+    quill.on("text-change", handler);
 
-        return () => {
-            quill.off("text-change", handler)
-        }
-    }, [socket, quill])
+    return () => {
+      quill.off("text-change", handler);
+    };
+  }, [socket, quill]);
 
-    const wrapperRef = useCallback((wrapper) => {
-        if (wrapper == null) return
-        wrapper.innerHTML = "";
-        const editor = document.createElement("div");
-        wrapper.append(editor);
-        const q = new Quill(editor, { theme: "snow", modules: {toolbar: TOOLBAR_OPTIONS}} );
-        q.disable();
-        q.setText("Loading...");
-        setQuill(q);
-        }, [])
-    return <div className="container" ref={wrapperRef}></div>;
+  const wrapperRef = useCallback((wrapper: HTMLDivElement) => {
+    if (wrapper == null) return;
+    wrapper.innerHTML = "";
+    const editor = document.createElement("div");
+    wrapper.append(editor);
+    const q = new Quill(editor, {
+      theme: "snow",
+      modules: { toolbar: TOOLBAR_OPTIONS },
+    });
+    q.disable();
+    q.setText("Loading...");
+    setQuill(q);
+  }, []);
+  return <div className="container" ref={wrapperRef}></div>;
 }
-
